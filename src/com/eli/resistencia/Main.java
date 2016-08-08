0b59f26d3c1ca4b1a2acb1ecfa26673f8beef8a4
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-08 08:23:13 -0500

    ELI: agrega funcion calcularArea

--- src/com/eli/resistencia/Main.java
+++ src/com/eli/resistencia/Main.java
@@ -3,7 +3,20 @@ package com.eli.resistencia;
 public class Main {
 
     public static void main(String[] args) {
-        
+
 	// write your code here
+        int Longitud= 1;
+        int diametro= 1;
+        double resistividadCobre=1.78e-8;
+        double resistividadSilicio=2300;
+
     }
+
+    public static double calcularArea(int diametro){
+        double pi=3.14;
+        double area=pi*diametro*diametro/4;
+        return area;
+    }
+
+    
 }
